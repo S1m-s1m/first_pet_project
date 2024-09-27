170ba6cef1e22605499b5e5abaca235bd1e0d673
S1m-s1m <2007kim.maksim@gmail.com>
2024-09-28 01:09:14 +0500

    prepare 6 commit

--- pet_project/celery.py
+++ pet_project/celery.py
@@ -1,14 +1,14 @@
-import sys
-
 import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pet_project.settings')
+
 app = Celery('pet_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+app.conf.broker_url = os.environ.get('REDIS_URL')  # Подключение к Redis
 app.conf.broker_connection_retry_on_startup = True
-
-
+app.conf.task_serializer = 'json'
+app.conf.result_backend = os.environ.get('REDIS_URL') 
 
