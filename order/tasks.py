5765499e4dfee4a55f94454c1639212aa53fd7ed
S1m-s1m <2007kim.maksim@gmail.com>
2024-10-01 09:37:55 +0500

    new commit

--- order/tasks.py
+++ order/tasks.py
@@ -20,23 +20,23 @@ def test_task():
 
 @shared_task(name='payment_completed')
 def payment_completed(order_id):
-    """
-    Задание по отправке уведомления по электронной почте
-    при успешной оплате заказа.
-    """
-    order = Order.objects.get(pk=order_id)
-    subject = f'My Shop – Invoice no. {order.pk}'
-    message = 'Please, find attached the invoice for your recent purchase.'
-    email = EmailMessage(subject, message, '2007kim.maksim@gmail.com', [order.email])
-    # сгенерировать PDF
-    html = render_to_string('admin/orders/order/pdf.html', {'order': order})
-    out = BytesIO()
-    stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'order/css/pdf.css')]
-    weasyprint.HTML(string=html).write_pdf(out,stylesheets=stylesheets)
-    # прикрепить PDF-файл
-    email.attach(f'order_{order.pk}.pdf',out.getvalue(),'application/pdf')
-    # отправить электронное письмо
-    email.send()
+    # """
+    # Задание по отправке уведомления по электронной почте
+    # при успешной оплате заказа.
+    # """
+    # order = Order.objects.get(pk=order_id)
+    # subject = f'My Shop – Invoice no. {order.pk}'
+    # message = 'Please, find attached the invoice for your recent purchase.'
+    # email = EmailMessage(subject, message, '2007kim.maksim@gmail.com', [order.email])
+    # # сгенерировать PDF
+    # html = render_to_string('admin/orders/order/pdf.html', {'order': order})
+    # out = BytesIO()
+    # stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'order/css/pdf.css')]
+    # weasyprint.HTML(string=html).write_pdf(out,stylesheets=stylesheets)
+    # # прикрепить PDF-файл
+    # email.attach(f'order_{order.pk}.pdf',out.getvalue(),'application/pdf')
+    # # отправить электронное письмо
+    # email.send()
 
     email = EmailMessage(subject='Good day', body='It is a second test message', from_email='2007kim.maksim@gmail.com', to=['2007kim.maksim@gmail.com'])
     email.send()
